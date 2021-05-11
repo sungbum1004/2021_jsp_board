3713ccb6e597c9531747d3394ed11c08c41bafe9
jhs512 <jangka512@gmail.com>
2021-05-11 23:30:59 +0900

    Rq 클래스 적용

---
+++ src/main/java/com/jhs/exam/exam2/http/Rq.java
@@ -0,0 +1,84 @@
+package com.jhs.exam.exam2.http;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Rq {
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
+	private boolean isInvalid = false;
+	private String controllerTypeName;
+	private String controllerName;
+	private String actionMethodName;
+
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		// 들어오는 파리미터를 UTF-8로 해석
+		try {
+			req.setCharacterEncoding("UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		// 서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
+		resp.setCharacterEncoding("UTF-8");
+
+		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
+		resp.setContentType("text/html; charset=UTF-8");
+
+		this.req = req;
+		this.resp = resp;
+
+		String requestUri = req.getRequestURI();
+		String[] requestUriBits = requestUri.split("/");
+
+		int minBitsCount = 5;
+
+		if (requestUriBits.length < minBitsCount) {
+			isInvalid = true;
+			return;
+		}
+
+		int controllerTypeNameIndex = 2;
+		int controllerNameIndex = 3;
+		int actionMethodNameIndex = 4;
+
+		this.controllerTypeName = requestUriBits[controllerTypeNameIndex];
+		this.controllerName = requestUriBits[controllerNameIndex];
+		this.actionMethodName = requestUriBits[actionMethodNameIndex];
+	}
+
+	public HttpServletRequest getReq() {
+		return req;
+	}
+
+	public boolean isInvalid() {
+		return isInvalid;
+	}
+
+	public String getControllerTypeName() {
+		return controllerTypeName;
+	}
+
+	public String getControllerName() {
+		return controllerName;
+	}
+
+	public String getActionMethodName() {
+		return actionMethodName;
+	}
+
+	public void print(String str) {
+		try {
+			resp.getWriter().append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void println(String str) {
+		print(str + "\n");
+	}
+}
--- src/main/java/com/jhs/exam/exam2/servlet/DispatcherServlet.java
+++ src/main/java/com/jhs/exam/exam2/servlet/DispatcherServlet.java
@@ -8,43 +8,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.jhs.exam.exam2.http.Rq;
 import com.jhs.mysqliutil.MysqlUtil;
 
 @WebServlet("/usr/*")
 public class DispatcherServlet extends HttpServlet {
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// 들어오는 파리미터를 UTF-8로 해석
-		req.setCharacterEncoding("UTF-8");
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+		Rq rq = new Rq(req, resp);
 
-		// 서블릿이 HTML 파일을 만들때 UTF-8 로 쓰기
-		resp.setCharacterEncoding("UTF-8");
-		
-		// HTML이 UTF-8 형식이라는 것을 브라우저에게 알린다.
-		resp.setContentType("text/html; charset=UTF-8");
+		if (rq.isInvalid()) {
+			rq.print("올바른 요청이 아닙니다.");
+		}
 
-		String requestUri = req.getRequestURI();
-		String[] requestUriBits = requestUri.split("/");
-
-		int minBitsCount = 5;
-		
-		if (requestUriBits.length < minBitsCount) {
-			resp.getWriter().append("올바른 요청이 아닙니다.");
-			return;
-		}		
-		
-		int controllerTypeNameIndex = 2;
-		int controllerNameIndex = 3;
-		int actionMethodNameIndex = 4;
-
-		String controllerTypeName = requestUriBits[controllerTypeNameIndex];
-		String controllerName = requestUriBits[controllerNameIndex];
-		String actionMethodName = requestUriBits[actionMethodNameIndex];
-		
-		resp.getWriter().append("controllerTypeName : " + controllerTypeName);
-		resp.getWriter().append("<br>");
-		resp.getWriter().append("controllerName : " + controllerName);
-		resp.getWriter().append("<br>");
-		resp.getWriter().append("actionMethodName : " + actionMethodName);
+		rq.println("controllerTypeName : " + rq.getControllerTypeName());
+		rq.println("<br>");
+		rq.println("controllerName : " + rq.getControllerName());
+		rq.println("<br>");
+		rq.println("actionMethodName : " + rq.getActionMethodName());
 
 		MysqlUtil.setDBInfo("localhost", "sbsst", "sbs123414", "jsp_board");
 		MysqlUtil.setDevMode(true);
